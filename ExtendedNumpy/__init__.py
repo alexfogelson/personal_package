5d820f7a3824604f5ff81f63c8df38ceae5f85c2
Alex Fogelson <fogelsonA@verizon.net>
2022-04-13 08:49:53 -0400

    Added matrix_tabulate and zero_print functions to ExtendedNumpy.

--- ExtendedNumpy/__init__.py
+++ ExtendedNumpy/__init__.py
@@ -10,3 +10,48 @@ def tabulate(func, hi, low = 0):
     
     '''
     return np.vectorize(func)(np.arange(low, hi))
+
+def matrix_tabulate(func, shape):
+    '''
+    Tabulates a new matrix using a given function.
+    
+    param func <function object> : A function from (int, int) -> Any.
+    param shape Tuple(int, 2): shape of the desired output
+    
+    '''
+
+    m, n = shape
+    indices = np.arange(m*n).reshape(m, n)
+    return np.vectorize(lambda i: func(i % m, int((i - i%m)/m)))(indices)
+
+def zero_print(X, precision = 2, **kwargs):
+    '''
+    Prints X as nicely formatted where elements close to zero are printed as "0"
+    
+    param X np.ndarray : Input matrix
+    param precision int (default 2) : Rounding precision
+    param atol, rtol float : Passed into np.isclose.
+    
+    '''
+    
+    assert len(X.shape) <= 2, "Currently printing higher dimensional arrays is not supported."
+
+    row_array = []
+
+    for row in X:
+        element_array = []
+        for element in row:
+            if (np.isclose(element, 0, **kwargs)):
+                element_array.append("0")
+            else:
+                element_array.append(str(round(element, precision)))
+        row_array.append(element_array)
+    
+    greatest_length = matrix_tabulate(lambda i,j: len(row_array[i][j]), X.shape).max()
+
+    row_array_padded = [[element + (greatest_length - len(element))*" " for element in row] for row in row_array]
+
+    final_output = "\n".join([" ".join(row) for row in row_array_padded])
+
+    print(final_output)
+
--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 Instructions on how to bundle this package were taken from here: https://opensource.com/article/21/11/packaging-python-setuptools
 
-To install the package, from the ```convenience``` directory, run the following:
+To build this package, from the ```convenience``` directory, run the following:
+
+```python3 -m build```
+
+To install this package, from the ```convenience``` directory, run the following:
+
+```python3 -m pip install dist/convenience-0.0.1.tar.gz```
+
+For copy and pasting, to do both:
 
 ```python3 -m build; python3 -m pip install dist/convenience-0.0.1.tar.gz```
-
